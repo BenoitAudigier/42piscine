dbdf4a962b2cfce6e8d5bb937697ae51387ba51d
Priscille Boissonnet <PriscilleBoissonnet@MacBook-Pro-de-Priscille.local>
2018-10-03 13:56:58 +0200

    update histogram algo

--- histogram.py
+++ histogram.py
@@ -13,18 +13,19 @@ def distribute_per_house(dict):
 
     houses = dict['Hogwarts House']
     for key in dict :
-        #1.check if the column is a quantitative one,meaning it's a subject
+        #1.check if the column is a quantitative one, meaning it contains marks
         if isNumericColumn(key,dict[key]):
             values_per_house = {}
-            #2. We scan the list dict[key] to build a new dict with as many keys as houses
+            #2. We scan the list dict[key] to build a new dict, key = house and values = values
             for i in range(len(dict[key])):
                 #3. We retrieve the house for the i-th value
                 current_house = houses[i]
-                if(dict[key][i]!=''):
-                    if current_house in values_per_house:
-                        values_per_house[current_house].append(dict[key][i])
-                    else :
-                        values_per_house[current_house]=[dict[key][i]]
+                
+#TEST ON GARDE TOUTES LES VALEURS, MEME LES NA, IL FAUT LES ENLEVER JUSTE AU MOMENT DE PLOT
+                if current_house in values_per_house:
+                    values_per_house[current_house].append(dict[key][i])
+                else :
+                    values_per_house[current_house]=[dict[key][i]]
 
             #4. add the new dict to the final result
             res[key]=values_per_house
@@ -39,7 +40,11 @@ def count(values_list,min,max,k):
 
     #1.create a count list, output of the algo
     count = [0]*k
-    #2. Scan the list to count the number of values in each interval
+    
+    #2. We remote na values
+    values_list = [i for i in values_list if i!='']
+
+    #3. Scan the list to count the number of values in each interval
     for i in values_list:
         if i==max:
             count[-1]+=1
@@ -69,9 +74,40 @@ def coord_y_axis(values_list,min,max,k):
         res.append(i)
     return res
 
-#input : dict with all columns (quantitaves or not)
+
+
+#input : dict with all (needed for the summary)
 #output : plots of one hist per field, with 4 lines on each : one per house
-def display(dict,k):
+def display_hist(name,dict,k):
+    per_house = distribute_per_house(dict)
+    summary = compute(dict)
+
+    #per 'matiere scolaire', we compute a plot
+    per_house_values = per_house[name]
+    min_global = summary[name]["Min"]
+    max_global = summary[name]["Max"]
+
+    x=coord_x_axis(min_global,max_global,k)
+    legend = []
+    #per house per matiere scolaire, we add a line on the plot
+    for house in per_house_values :
+        y=coord_y_axis(per_house_values[house],min_global,max_global,k)
+        plt.plot(x,y)
+        legend.append(house)
+    plt.title(name)
+    plt.legend(legend)
+
+    plt.xlim(min_global,max_global)
+    plt.show()
+
+
+
+
+
+
+#input : dict with all (needed for the summary)
+#output : plots of one hist per field, with 4 lines on each : one per house
+def display_hist_all(dict,k):
     #dict with the house distribution of the quantitative values
     per_house = distribute_per_house(dict)
     summary = compute(dict)
@@ -98,4 +134,14 @@ def display(dict,k):
 
 
 dict = getDataSet("dataset_train.csv")
-display(dict,6)
+#dict = distribute_per_house(dict)
+
+compt = 1
+for k in dict : 
+    if(k!="Index" and isNumericColumn(k, dict[k])):
+        print(compt)
+        print(k)
+        if(compt<10):
+            display_hist(k,dict,16)
+        compt +=1
+    
