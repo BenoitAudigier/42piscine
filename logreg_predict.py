37c3fce223dcce8326c0c21adeb56928005e0ecf
BenoitAudigier <benoit.audigier@polytechnique.edu>
2018-10-03 16:32:56 +0200

    Finishing prediction and tests

---
+++ logreg_predict.py
@@ -0,0 +1,23 @@
+import logreg_train as lt
+import numpy as np
+import csv
+
+def predictFromCSV(fileName):
+	# Get data
+	X, X_1 = lt.getData(fileName, training = False)
+
+	# get theta
+	theta = lt.readTheta()
+
+	# predict
+	houseNumberConversionVect = np.vectorize(lt.houseNumberConversion)
+	preds = houseNumberConversionVect(lt.predictMat(theta, X_1))
+
+	# Adding the index
+	preds = np.vstack((np.array(range(len(preds))), preds)).T
+
+	# Write the csv
+	with open("results.csv", "w") as output:
+	    writer = csv.writer(output, lineterminator='\n')
+	    writer.writerows([["Index", "Hogwarts House"]])
+	    writer.writerows(preds)
--- logreg_train.py
+++ logreg_train.py
@@ -1,23 +1,65 @@
+# TODO be better on missing values
+# Source: http://blog.datumbox.com/machine-learning-tutorial-the-multinomial-logistic-regression-softmax-regression/
+
 from dataHandler import getDataSet
 from computation import isNumericColumn
 import numpy as np
+import pickle
 
+# For preformating the numbers (cause of Na's)
 def tryToConvToNum(s):
     try:
         return(float(s))
     except ValueError:
         return(None) # will result in nan
+# Scaling (using old or new values if training or not)
+def scalingData(X, training):
+    # Centering/Scaling variables
+    # Storing/Retrieving means std devs
+    if training:
+        ms = []
+        stdevs = []
+    else:
+        ms, stdevs = readScalingParams() # Might throw exception
+    # Applying the rescaling
+    for i in range(X.shape[0]):
+        row = X[i, :]
+        if training: # Compute and keeping the scaling
+            # Computing for the row the mean and std deviation
+            # stdev = np.std(row[np.invert(np.isnan(row))])
+            # m = np.mean(row[np.invert(np.isnan(row))])
+            stdev = np.nanstd(row)
+            m = np.nanmean(row)
+            ms += [m]
+            stdevs += [stdev]
+        else: # Read the scaling
+            m = ms[i]
+            stdev = stdevs[i]
+        # Applying
+        for j in range(len(row)):
+            if(np.isnan(row[j])):
+                row[j] = 0 # mean of the new array
+            else:
+                row[j] = (row[j] - m)/stdev
+        X[i, :] = row
+    # Saving the parameters
+    writeScalingParams(ms, stdevs)
+    return(X)
 
 
 
-# Return matrix of values and vector of labels
-def getData(fileName = None):
+# Return matrix of values without and with one for biais and vector of labels
+def getData(fileName = None, training = True):
+
+
     # Reading file
     try:
         ds = getDataSet(fileName)
     except ValueError as e:
         print("Error: " + str(e))
         return(None)
+
+
 
     # Isolate numeric columns
     dsNum = {}
@@ -31,30 +73,22 @@ def getData(fileName = None):
     # Replacing NA by None and converting to float
     X = np.vectorize(tryToConvToNum)(dsMat)
 
-    # Centering/Scaling varaibles
-    for i in range(X.shape[0]):
-        row = X[i, :]
-        # Computing for the row the mean and std deviation
-        # stdev = np.std(row[np.invert(np.isnan(row))])
-        # m = np.mean(row[np.invert(np.isnan(row))])
-        stdev = np.nanstd(row)
-        m = np.nanmean(row)
-        # Applying
-        for j in range(len(row)):
-            if(np.isnan(row[j])):
-                row[j] = 0 # mean of the new array
-            else:
-                row[j] = (row[j] - m)/stdev
-        X[i, :] = row
 
 
+    # Scaling
+    X = scalingData(X, training)
+
+
+    # Adding ones for biais
     X_1 = np.concatenate((np.array(np.ones((1, X.shape[1]))), X))
 
-    y_str = np.array(ds["Hogwarts House"])
-    y_num = np.array([houseNumberConversion(y_i) for y_i in y_str])
-
-    return X, X_1, y_str, y_num
-
+    if training:
+        # Dealing with labels
+        y_str = np.array(ds["Hogwarts House"])
+        y_num = np.array([houseNumberConversion(y_i) for y_i in y_str])
+        return X, X_1, y_str, y_num
+    else:
+        return X, X_1
 # Both ways, raises exception if unknown
 def houseNumberConversion(toConv):
     houseToNum = {"Gryffindor":0, "Slytherin":1, "Ravenclaw":2, "Hufflepuff":3}
@@ -71,41 +105,63 @@ def houseNumberConversion(toConv):
         pass
     raise ValueError("Unknown house or number given to houseNumberConversion")
 
-def main(fileName = None):
-    # Getting data in nice way
-    X, X_1, y_str, y_num = getData(fileName)
 
-    # Defining variables
+# Main function, could've used an object but since no use of inheritance, and storage smaller if only the parameters are stored ..
+def train(fileName = None, verbatim = True):
+
+    # Getting data in nice way
+    X, X_1, y_str, y_num = getData(fileName, training = True)
+
+    # Defining variables used
     k = 4 # Number of categories
     n = X.shape[0] # Number of explicative variables
     m = X.shape[1] # Number of observations
+    trainingPercentage = .8
     alpha = .01 # Learning rate
     iteration_number = 200 # Iteration number
     # iteration_number = 100 # Iteration number
+
+
+
+    # Separation into train and validation set
+    trainLength = int(trainingPercentage * m)
+    testLength = m - trainingPercentage
+    indexTrain = np.sort(np.random.choice(range(m), trainLength, replace = False))
+    indexVal = np.array(list(set(range(m)) - set(indexTrain)))
+
+    X_train = X[:,indexTrain]
+    X_train_1 = X_1[:,indexTrain]
+    y_train_str = y_str[indexTrain]
+    y_train_num = y_num[indexTrain]
+
+    X_val = X[:,indexVal]
+    X_val_1 = X_1[:,indexVal]
+    y_val_str = y_str[indexVal]
+    y_val_num = y_num[indexVal]
 
     # Initializing weights
     theta = np.zeros((n+1, k))
 
     for i in range(iteration_number):
-        if(i % 25 == 0):
-            print("\nIteration number" + str(i))
-            print("Loss: " + str(loss(theta, X_1, y_num, m)))
+        if(i % 25 == 0 and verbatim):
+            print("\nIteration number " + str(i))
+            print("Loss: " + str(loss(theta, X_train_1, y_train_num)))
+            print("Training accuracy: " + str(calcAccuracy(predictMat(theta, X_train_1), y_train_num)))
+            print("Validation accuracy: " + str(calcAccuracy(predictMat(theta, X_val_1), y_val_num)))
 
         for j in range(k):
-            theta[:, j] = theta[:, j] - alpha * gradLoss(j, theta, X_1, y_num, m, k)
+            theta[:, j] = theta[:, j] - alpha * gradLoss(j, theta, X_train_1, y_train_num)
 
-    print("Train prediction:")
-    for i in range(50):
-        print("Dude number " + str(i))
-        predict(theta, X_1[:, i])
-        print('Reality: ' + y_str[i])
-        print("\n")
+    # Saving stuff
+    writeTheta(theta)
+
 
 
 # Computes loss for given parameters
-def loss(theta, X_1, y_num, m):
+def loss(theta, X_1, y_num):
     J = 0
-    for i in range(m):
+    local_m = X_1.shape[1]
+    for i in range(local_m):
 
         x_i = X_1[:, i]
         theta_j = theta[:,y_num[i]] # Extracting the theta corresponding to the class of x_i
@@ -113,9 +169,20 @@ def loss(theta, X_1, y_num, m):
         # Adding the loss of the individual
         J += np.log(hypoFun(theta_j, theta, x_i))
     # Averaging and inverting sign
-    J = -J/m
+    J = -J/local_m
     return J
-
+# Computes the gradient of the loss function
+def gradLoss(j, theta, X_1, y_num):
+    theta_j = theta[:, j]
+    DJ = 0
+    local_m = X_1.shape[1]
+    for i in range(local_m):
+        x_i = X_1[:, i]
+        # Adding the gradient loss of the individual
+        DJ += x_i*(abs(j==y_num[i]) - hypoFun(theta_j, theta, x_i))
+    # Averaging and inverting sign
+    DJ = -DJ/local_m
+    return DJ
 # computes the hypothesis function
 def hypoFun(theta_j, theta, x_i):
         return(
@@ -125,27 +192,55 @@ def hypoFun(theta_j, theta, x_i):
         )
 
 
-def gradLoss(j, theta, X_1, y_num, m, k):
-    theta_j = theta[:, j]
-    DJ = 0
-    for i in range(m):
-        x_i = X_1[:, i]
-        # Adding the gradient loss of the individual
-        DJ += x_i*(abs(j==y_num[i]) - hypoFun(theta_j, theta, x_i))
-    # Averaging and inverting sign
-    DJ = -DJ/m
-    return DJ
 
-
-
-# X_test must be of shape (13, n) like the original data X
+# Predict for one guy
 def predict(theta, x_i_1):
+    prob = -1
+    housePred = -1
     for house in range(4):
-        print(houseNumberConversion(house))
-        print(hypoFun(theta[:, house], theta, x_i_1))
+        current_prob = hypoFun(theta[:, house], theta, x_i_1)
+        if(current_prob > prob):
+            prob = current_prob
+            housePred = house
+    return(housePred)
+# X_topred_1 must be of shape (13 + 1, n) like the original data X
+def predictMat(theta, X_topred_1):
+    preds = np.array([predict(theta, X_topred_1[:, i]) for i in range(X_topred_1.shape[1])])
+    return(preds)
+# Percentage of right guesses, throws exception if mismatched vectors
+def calcAccuracy(y_pred, y_real):
+    if(len(y_pred) != len(y_real)):
+        raise ValueError("Trying to compute accuracy of unmatching size vectors (real vs prediction)")
+    return(sum(y_real == y_pred)/len(y_pred))
+
+
+
+# pickling a dict
+def writeTheta(theta):
+    with open("params/theta.P", "wb") as fp:
+        pickle.dump(theta, fp)
+# unpickling a dict
+def readTheta():
+    with open("params/theta.P", "rb") as fp: # Pickling
+        theta = pickle.load(fp)
+    return(theta)
+# pickling a dict
+def writeScalingParams(ms, stds):
+    tmp = {"ms":ms, "stds":stds}
+    with open("params/scales.P", "wb") as fp:
+        pickle.dump(tmp, fp)
+# unpickling a dict
+def readScalingParams():
+    with open("params/scales.P", "rb") as fp: # Pickling
+        tmp = pickle.load(fp)
+    try:
+        ms = tmp["ms"]
+        stds = tmp["stds"]
+        return ms, stds
+    except:
+        print('Impossible to retrieve the saved value from last training.')
+        return(False)
+
 
     # return(res, np.array([houseNumberConversion(y_i) for y_i in res]))
 
-# main(fileName = "dataset_train.csv")
-#TODO clean up that shit, add separation train and test, compute prediction, be better on missing values, comment
-# Source: http://blog.datumbox.com/machine-learning-tutorial-the-multinomial-logistic-regression-softmax-regression/
